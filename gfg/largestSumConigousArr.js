25598a24ae6b89ed56afd6635f45918b550d455f
Rishant Agarwal <bindasrishant@gmail.com>
2016-08-09 15:00:17 +0900

    fixing bug

--- gfg/largestSumConigousArr.js
+++ gfg/largestSumConigousArr.js
@@ -2,31 +2,32 @@
 
 var arr = [-1,-2,-3,1,2,3,4,5,0,6,-3,0,-4,1,2,3,4,5,0,6];
 // arr = [-1,-1,-4,-6,-2];
-arr = [-2, -3, 4, -1, -2, 1, 5, -3];
+// arr = [-2, -3, 4, -1, -2, 1, 5, -3];
+arr = [20,-3,4,-1,-2,1,5,-3];
 
 
-//O(n) based solution Dynamic Programming 
+//O(n) based solution Dynamic Programming
 function getResult(arr){
   var prevPosSum = 0,prevNegSum=0,currPosSum=0,currNegSum= 0,max = 0;
   var result = [];
   var index = 0;
   while(index<arr.length){
     while(arr[index]>=0){
-      // console.log(arr[index],"P");
+      console.log(arr[index],"P");
       currPosSum+= arr[index];
       index++;
     }
 
     while(arr[index]<=0){
-        // console.log(arr[index],"N");
+        console.log(arr[index],"N");
         currNegSum+= arr[index];
         index++;
     }
 
-    max = Math.max(prevPosSum+prevNegSum+currPosSum,currPosSum,prevPosSum);
+    max = Math.max(prevPosSum+prevNegSum+currPosSum,currPosSum,prevPosSum,max);
     prevPosSum = currPosSum;
     prevNegSum = currNegSum;
-    // console.log("#",currPosSum,currNegSum,max);
+    console.log("#",currPosSum,currNegSum,max);
     currNegSum = currPosSum = 0;
 
   }
