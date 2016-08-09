db05af39c25b4fbef6f7eed1255f8fe50d2dab3c
Rishant Agarwal <bindasrishant@gmail.com>
2016-08-09 15:02:41 +0900

    http://www.geeksforgeeks.org/largest-sum-contiguous-subarray/

--- gfg/largestSumConigousArr.js
+++ gfg/largestSumConigousArr.js
@@ -3,7 +3,8 @@
 var arr = [-1,-2,-3,1,2,3,4,5,0,6,-3,0,-4,1,2,3,4,5,0,6];
 // arr = [-1,-1,-4,-6,-2];
 // arr = [-2, -3, 4, -1, -2, 1, 5, -3];
-arr = [20,-3,4,-1,-2,1,5,-3];
+// arr = [20,-3,4,-1,-2,1,5,-3];
+arr = [-2,-1];
 
 
 //O(n) based solution Dynamic Programming
@@ -13,13 +14,13 @@ function getResult(arr){
   var index = 0;
   while(index<arr.length){
     while(arr[index]>=0){
-      console.log(arr[index],"P");
+      // console.log(arr[index],"P");
       currPosSum+= arr[index];
       index++;
     }
 
     while(arr[index]<=0){
-        console.log(arr[index],"N");
+        // console.log(arr[index],"N");
         currNegSum+= arr[index];
         index++;
     }
@@ -27,7 +28,7 @@ function getResult(arr){
     max = Math.max(prevPosSum+prevNegSum+currPosSum,currPosSum,prevPosSum,max);
     prevPosSum = currPosSum;
     prevNegSum = currNegSum;
-    console.log("#",currPosSum,currNegSum,max);
+    // console.log("#",currPosSum,currNegSum,max);
     currNegSum = currPosSum = 0;
 
   }
