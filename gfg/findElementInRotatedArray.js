ef900305e709e974c5738e51ab7eceb07fe75669
Rishant Agarwal <bindasrishant@gmail.com>
2016-08-10 15:13:06 +0900

    http://www.geeksforgeeks.org/search-an-element-in-a-sorted-and-pivoted-array/

--- gfg/findElementInRotatedArray.js
+++ gfg/findElementInRotatedArray.js
@@ -1,39 +1,60 @@
 // http://www.geeksforgeeks.org/find-the-missing-number/
-
+// Assuming array has been rotated in Clockwise order
+// [5,4,3,2,1] becomes [2,1,5,4,3]
 var arr = [2,1,5,4,3];
 
-// Global variables
-var n,start=-1,end=-1;
-var isIncreasing = false;
+function getResult(arr,num){
+
+  var pivot = getPivot(arr);
+  var isIncreasing;
+  var high,low;
+  var n = arr.length;
 
 
-function getResult(arr){
-  initializeStuff(arr);
 
-  return arr;
+  if(arr[pivot+1]>arr[pivot+2] && arr[pivot+2]>arr[pivot+3]){isIncreasing = 0;}
+  if(arr[pivot+1]<arr[pivot+2] && arr[pivot+2]<arr[pivot+3]){isIncreasing = 1;}
+
+   if(isIncreasing){
+     if(num > arr[pivot] || num < arr[pivot+1]) return -1;
+     else if(num <= arr[n-1]){low = pivot+1; high = n-1;}
+     else if(num > arr[n-1]){low = 0; high = pivot;}
+     }
+   else {
+     if(num < arr[pivot] || num > arr[pivot+1]) return -1;
+     else if(num >= arr[n-1]){low = pivot+1; high = n-1;}
+     else if(num < arr[n-1]){low = 0; high = pivot;}
+   }
+
+   console.log(isIncreasing);
+
+  return binarySearch(arr,low,high,num,isIncreasing);
+}
+
+function binarySearch(arr,low,high,num,isIncreasing){
+  var mid = low + Math.floor(high-low)/2;
+
+  if(num > arr[mid]) {
+    if(isIncreasing){return binarySearch(arr,mid+1,high,num,isIncreasing);}
+    else { return binarySearch(arr,low,mid-1,num,isIncreasing);}
+  }
+  else if(num < arr[mid]){
+    if(!isIncreasing){return binarySearch(arr,mid+1,high,num,isIncreasing);}
+    else { return binarySearch(arr,low,mid-1,num,isIncreasing);}
+  }
+  else if(num == arr[mid]){return mid;}
+  else if(high<=low){return -1;}
 }
 
 
-function find(num){
-  
-}
 
-function initializeStuff(arr){
-  n = arr.length;
+function getPivot(arr){
+  var n= arr.length;
   for(var i=0;i<n-2;i++){
-    if((arr[i] < arr[i+1] && arr[i+1] > arr[i+2]) || (arr[i] > arr[i+1] && arr[i+1] < arr[i+2])){
-      start = i+1;
-      end = i;
-    }
+    if(arr[i]<arr[i+1] && arr[i+1]>arr[i+2]){return i+1;}
+    if(arr[i]>arr[i+1] && arr[i+1]<arr[i+2]){return i+1;}
   }
 
-if(start == -1 && end == -1){
-  start=0;
-  end = n;
 }
 
-  console.log(n,start,end);
-}
-
-
-console.log(getResult(arr));
+console.log(getResult(arr,5));
